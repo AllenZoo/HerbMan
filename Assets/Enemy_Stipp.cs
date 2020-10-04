e064b685e79a43d0f086983f47d96c34ca2c17c3
Allen Zhu <allenzhu2000@hotmail.com>
2020-10-03 21:59:27 -0700

    Added Path0000.png, Tree.png, and Enemy_Stipp

---
+++ Assets/Enemy_Stipp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Stipp : MonoBehaviour
+{
+    private float health;
+    private float moveSpeed;
+
+    private Enemy_Base enemyBase;
+   
+
+    private void Awake()
+    {
+        enemyBase = GetComponent<Enemy_Base>();
+        health = enemyBase.GetHealth();
+        moveSpeed = enemyBase.GetMoveSpeed();
+    }
+    
+
+
+}
---
+++ Assets/Scripts/Enemy_Base.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Base : MonoBehaviour
+{
+    [SerializeField] private float health;
+    [SerializeField] private float moveSpeed;
+
+    public float GetHealth()
+    {
+        return health;
+    }
+    public void SubtractHealth(float num)
+    {
+        this.health -= num;
+    }
+    public void AddHealth(float num)
+    {
+        this.health += num;
+    }
+    public void SetHealth(float num)
+    {
+        this.health = num;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+    public void SubtractMoveSpeed(float num)
+    {
+        this.moveSpeed -= num;
+    }
+    public void AddMoveSpeed(float num)
+    {
+        this.moveSpeed += num;
+    }
+    public void SetMoveSpeed(float num)
+    {
+        this.moveSpeed = num;
+    }
+}
